6c5d7ce23ba22337861bed7be5246ec7479a04ba
arturasVis <artvis44@gmail.com>
2024-03-08 13:22:50 +0000

    Changed to https

--- src/weather_Api.js
+++ src/weather_Api.js
@@ -3,10 +3,10 @@ export function getCurrentAuto(location="",latitude="", longitude=""){
     let fetchURL=""
     if(location==="")
     {
-        fetchURL=`http://api.weatherapi.com/v1/current.json?key=bc4649de6fbf447e84f171804240403&q=${latitude},${longitude}&aqi=no`
+        fetchURL=`https://api.weatherapi.com/v1/current.json?key=bc4649de6fbf447e84f171804240403&q=${latitude},${longitude}&aqi=no`
     }
     else{
-        fetchURL=`http://api.weatherapi.com/v1/current.json?key=bc4649de6fbf447e84f171804240403&q=${location}&aqi=no`
+        fetchURL=`https://api.weatherapi.com/v1/current.json?key=bc4649de6fbf447e84f171804240403&q=${location}&aqi=no`
     }
     return fetch(fetchURL,{mode:"cors"})
     .then(function(response){
@@ -22,10 +22,10 @@ export function getForecast(location="",latitude="", longitude=""){
     let fetchURL=""
     if(location==="")
     {
-        fetchURL=`http://api.weatherapi.com/v1/forecast.json?key=bc4649de6fbf447e84f171804240403&q=${latitude},${longitude}&days=7&aqi=no&alerts=no`
+        fetchURL=`https://api.weatherapi.com/v1/forecast.json?key=bc4649de6fbf447e84f171804240403&q=${latitude},${longitude}&days=7&aqi=no&alerts=no`
     }
     else{
-        fetchURL=`http://api.weatherapi.com/v1/forecast.json?key=bc4649de6fbf447e84f171804240403&q=${location}&days=7&aqi=no&alerts=no`
+        fetchURL=`https://api.weatherapi.com/v1/forecast.json?key=bc4649de6fbf447e84f171804240403&q=${location}&days=7&aqi=no&alerts=no`
     }
     return fetch(fetchURL,{mode:"cors"})
     .then(function(response){
